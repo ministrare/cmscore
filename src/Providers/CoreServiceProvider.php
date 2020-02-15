ca32682e732c7c0774f74bb8a60c2a90f466f0de
Lesley Forn <ministrare@hotmail.com>
2020-02-15 13:33:39 +0100

    initial commit

---
+++ src/Providers/CoreServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Ministrare\Cmscore\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
